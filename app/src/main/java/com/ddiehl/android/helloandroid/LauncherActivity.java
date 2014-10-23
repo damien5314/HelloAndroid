bbbc3359d4ea9820344e2c372ce2f9a6dedc42f0
damien5314 <damien5314@gmail.com>
2014-10-22 22:32:17 -0400

    Initial commit

---
+++ app/src/main/java/com/ddiehl/android/helloandroid/LauncherActivity.java
@@ -0,0 +1,15 @@
+package com.ddiehl.android.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+
+}
